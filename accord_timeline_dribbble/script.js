6ccec4e54dc289fce482648355b47829648f4be1
jasin1 <jasin.tairaidrissi@gmail.com>
2023-10-30 08:29:42 +0100

    added a lot of code

---
+++ accord_timeline_dribbble/script.js
@@ -0,0 +1,14 @@
+
+
+const checkBox = document.querySelectorAll('.eval-check-inside');
+
+const css_of_after = window.getComputedStyle(checkBox, ':before');
+
+console.log(css_of_after.position);
+
+
+//-------------- calculate dates intervals -------------- //
+
+const startDate = 0;
+
+const endDate = 10;
